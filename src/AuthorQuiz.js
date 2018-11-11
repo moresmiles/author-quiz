daf7492e2272b7a7f86f2f430bbe681a63a9c544
github username <github email address>
2018-11-11 14:33:04 +0200

    initial commit

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './AuthorQuiz.css';
+import './bootstrap.min.css'
+class AuthorQuiz extends Component {
+  render() {
+    return (
+      <div className="AuthorQuiz">
+        AuthorQuiz
+      </div>
+    );
+  }
+}
+
+export default AuthorQuiz;
