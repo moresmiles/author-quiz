1779f5712591f0c97ee832f1b66594d576415bd6
github username <github email address>
2018-11-12 14:21:48 +0200

    highlightColor function

--- src/Turn.js
+++ src/Turn.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import Book from './Book';
 
-const Turn = ({author, books}) => {
+const Turn = ({author, books, highlight}) => {
+  const highlightColor = (highlight) => {
+    const mapping = {
+      'none': '',
+      'correct': 'green',
+      'wrong': 'red'
+    };
+    return mapping[highlight]
+  }
+  
   return (
-    <div className="row turn" style={{backgroundColor: "white"}}>
+    <div className="row turn" style={{backgroundColor: highlightColor(highlight)}}>
       <div className="col-4 offset-1">
         <img src={author.imageUrl} className="authorImage" alt="Author"/>
       </div>
