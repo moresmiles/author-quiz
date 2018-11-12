d5e67f32d80678343b43ccd89252a3df31389dd1
github username <github email address>
2018-11-12 14:13:47 +0200

    changed from class component to function, and passed props to Turn

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
-import Hero from './Hero'
+import React from 'react';
+import Hero from './Hero';
+import Turn from './Turn';
 import './AuthorQuiz.css';
 import './bootstrap.min.css'
 
-class AuthorQuiz extends Component {
-  render() {
+const AuthorQuiz = ({turnData}) => {
     return (
       <div className="container-fluid">
         <Hero />
+        <Turn {...turnData}/>
      </div>
     );
   }
-}
+
 
 export default AuthorQuiz;
