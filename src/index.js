1b252c92d6ae0a55da99de46f55dd6ab9cdc01d1
github username <github email address>
2018-11-12 14:12:46 +0200

    authors array

--- src/index.js
+++ src/index.js
@@ -2,11 +2,51 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<AuthorQuiz />, document.getElementById('root'));
+const authors =[
+    {
+    name: 'Mark Twain',
+    imageUrl:'images/authors/marktwain.jpg',
+    imageSource:'Wikimedia Commons',
+    books: ['The Adventures of Huckleberry Finn']
+    },
+    {
+    name: 'Joseph Conrad',
+    imageUrl:'images/authors/josephconrad.jpeg',
+    imageSource:'Wikimedia Commons',
+    books: ['Heart of darkness']
+    },
+    {
+    name: 'J K Rowling',
+    imageUrl:'images/authors/jkrowling.jpeg',
+    imageSource:'Wikimedia Commons',
+    imageAttribution:'Daniel Ogren',
+    books: ['Harry Porter and sorcerers stone']
+    },
+    {
+    name: 'Stephen King',
+    imageUrl:'images/authors/stephenking.jpeg',
+    imageSource:'Wikimedia Commons',
+    imageAttribution:'Pingiun',
+    books: ['The shining', 'IT']
+    },
+    {
+    name: 'Charlse Dickens',
+    imageUrl:'images/authors/charlesdickens.jpeg',
+    imageSource:'Wikimedia Commons',
+    books: ['David Copperfield', 'A Tale of Two Cities']
+    },
+    {
+    name: 'William Shakespear',
+    imageUrl:'images/authors/williamshakespear.jpeg',
+    imageSource:'Wikimedia Commons',
+    books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
+    }
+]
+
+
+ReactDOM.render(<AuthorQuiz {...state} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
