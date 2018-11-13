f4e8ab391deff855e9e0a1ceceb1a91343e99238
github username <github email address>
2018-11-13 15:04:17 +0200

    Link to AddAuthorForm Component

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from './Hero';
 import Turn from './Turn';
+import {Link} from 'react-router-dom';
 import './AuthorQuiz.css';
 import './bootstrap.min.css'
 
@@ -9,6 +10,7 @@ const AuthorQuiz = ({turnData, highlight, onAnswerSelect}) => {
       <div className="container-fluid">
         <Hero />
         <Turn {...turnData} highlight={highlight} onAnswerSelect={onAnswerSelect}/>
+        <p> <Link to="/add"> Add an Author </Link> </p>
      </div>
     );
   }
